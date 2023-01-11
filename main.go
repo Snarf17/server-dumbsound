054714f6212aa9c6a46b49c075bced9252423fa1
Gusnanda1 <ida.wikananda@student.president.ac.id>
2023-01-11 12:13:28 +0700

    backend

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"dumbsound/database"
 	"dumbsound/pkg/mysql"
 	"dumbsound/routes"
-	"fmt"
+	
 	"net/http"
+	"os"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -16,6 +18,9 @@ func main() {
 	r := mux.NewRouter()
 	routes.RouteInit(r.PathPrefix("/api/v1").Subrouter())
 	r.PathPrefix("/uploads").Handler(http.StripPrefix("/uploads/", http.FileServer(http.Dir("./uploads"))))
-	fmt.Println("server running localhost:8000")
-	http.ListenAndServe("localhost:8000", r)
+	var AllowedHeaders = handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	var AllowedMethods = handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS", "PATCH", "DELETE"})
+	var AllowedOrigins = handlers.AllowedOrigins([]string{"*"})
+	var port = os.Getenv("PORT")
+	http.ListenAndServe(":"+port, handlers.CORS(AllowedHeaders, AllowedMethods, AllowedOrigins)(r))
 }
