16cef9bd4ab81b27f31edaa138dfaa036518de7c
frans <afriandifrans@gmail.com>
2023-02-02 20:16:10 +0700

    update

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -12,7 +12,7 @@ var DB *gorm.DB
 func DatabaseInit() {
 	var err error
 	// dsn := "root:@tcp(127.0.0.1:3306)/dumbsound?charset=utf8mb4&parseTime=True&loc=Local"
-	dsn := "root:qDyWAxa56z04OgybAZaN@tcp(containers-us-west-189.railway.app:6092)/railway?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:qDyWAxa56z04OgybAZaN@containers-us-west-189.railway.app:6092/railway?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
